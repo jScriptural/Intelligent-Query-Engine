1b264cc12b81e11a16917d8eeee9a6950e725a22
Yohanan <isonguyojohn015@gmail.com>
2026-04-21 00:42:48 +0100

    feat: implement profile model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,4 +1,31 @@
- package models
- 
- 
- 
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Profile struct {
+	ID                 uuid.UUID `json:"id"`
+	Name               string    `json:"name"`
+	Gender             string    `json:"gender"`
+	GenderProbability  float64   `json:"gender_probability"`
+	Age                int       `json:"age"`
+	AgeGroup           string    `json:"age_group"`
+	CountryID          string    `json:"country_id"`
+	CountryName        string    `json:"country_name"`
+	CountryProbability float64   `json:"country_probability"`
+	CreatedAt          time.Time `json:"created_at"`
+}
+
+type Response struct {
+	Status string  `json:"status"`
+	Page   int     `json:"page"`
+	Limit  int     `json:"limit"`
+	Data   Profile `json:"data"`
+}
+
+type ErrResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
--- store/store.go
+++ store/store.go
@@ -1,4 +1,8 @@
 package store  
+
+import (
+	_ "modernc.org/sqlite"
+)
   
   
   
