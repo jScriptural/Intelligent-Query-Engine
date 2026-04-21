2107564c5be8487ede9ccedf92c0eb8462289c6f
Yohanan <isonguyojohn015@gmail.com>
2026-04-21 12:48:51 +0100

    feat: add CORS middleware for cross-origin request

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,3 +1,25 @@
-package mw
+package middleware
+
+import (
+	"net/http"
+	"log"
+)
+
+
+func CORS(h http.Handler, config map[string]string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("clientIP: %v\n",r.RemoteAddr)
+		for k,v := range config {
+			w.Header().Set(k,v);
+		}
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return;
+		}
+		h.ServeHTTP(w,r)
+	})
+}
+
 
 
