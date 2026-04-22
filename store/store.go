71d9f8a3068c707f584f7a2517ef7f30fc95f9a7
Yohanan <isonguyojohn015@gmail.com>
2026-04-22 08:46:02 +0100

    feat: implement dynamic filtering and pagination in GetProfiles

--- store/store.go
+++ store/store.go
@@ -3,13 +3,18 @@ package store
 import (
 	"database/sql"
 	"encoding/json"
-	//"fmt"
+	"fmt"
+	"context"
 	"github.com/google/uuid"
 	"intelliqe/internal/models"
 	"log"
 	_ "modernc.org/sqlite"
+	"net/url"
 	"os"
+	"strings"
+	"errors"
 	"time"
+	//"strconv"
 )
 
 type DBHandler struct {
@@ -120,4 +125,107 @@ func seedDB(db *sql.DB, seedfile string) error {
 
 	tx.Commit()
 	return nil
+}
+
+func (d *DBHandler) GetProfiles(ctx context.Context, q url.Values, page,limit int) ([]*models.Profile, int, error) {
+	var (
+		wc strings.Builder
+		qc strings.Builder
+	)
+	
+	qc.WriteString(`SELECT id,name,gender,gender_probability,age,age_group,country_id,country_name,country_probability,created_at
+FROM profile`)
+wc.WriteString(" WHERE 1=1")
+
+	var args []any
+	for k, v := range q {
+		//k = strings.ToLower(k)
+		val := v[0];
+		switch {
+		case k == "min_age":
+			wc.WriteString(" AND age >= ?")
+			args = append(args,val)
+		case k == "max_age":
+			wc.WriteString(" AND age <= ?")
+			args = append(args,val)
+		case k == "min_gender_probability":
+			wc.WriteString(" AND gender_probability >= ?")
+			args = append(args,val)
+		case k == "gender":
+			wc.WriteString(" AND gender = ?")
+		  val = strings.ToLower(val)
+			args = append(args,val)
+		case k == "country_id":
+			wc.WriteString(" AND country_id = ?")
+			args = append(args,strings.ToUpper(val))
+		case k == "age_group":
+			wc.WriteString(" AND age_group = ?")
+		  val = strings.ToLower(val)
+			args = append(args,val)
+		case k == "min_country_probability":
+			wc.WriteString(" AND country_probability >= ?");
+			args = append(args,val);
+		}
+	}
+
+	totalCount := 0;
+	sqlCount := "SELECT COUNT(*) FROM profile"+wc.String();
+
+	err := d.DB.QueryRowContext(ctx,sqlCount,args...).Scan(&totalCount);
+	if err != nil {
+		return nil,0,fmt.Errorf("GetProfiles: %w",err);
+	}
+
+
+	qc.WriteString(wc.String());
+	if q.Has("sort_by") && q.Has("order") {
+		sort_by := strings.ToLower(q.Get("sort_by"));
+		order := q.Get("order");
+		s := fmt.Sprintf(" ORDER BY %s %s",sort_by,order)
+		qc.WriteString(s)
+	}
+
+
+
+	offset := (page-1) * limit;
+	qc.WriteString(fmt.Sprintf(" LIMIT %v OFFSET %v",limit, offset))
+
+	qc.WriteString(";")
+	stmt := qc.String();
+
+	rows,err := d.DB.QueryContext(ctx,stmt,args...);
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows){
+			return nil,0,fmt.Errorf("GetProfiles: %w",models.ErrNotFound);
+		}
+	
+		return nil,0,fmt.Errorf("GetProfiles: %w",err);
+	}
+	defer rows.Close();
+
+	p := []*models.Profile{}
+	for rows.Next() {
+		t := models.Profile{};
+		rows.Scan(
+			&t.ID,
+			&t.Name,
+			&t.Gender,
+			&t.GenderProbability,
+			&t.Age,
+			&t.AgeGroup,
+			&t.CountryID,
+			&t.CountryName,
+			&t.CountryProbability,
+			&t.CreatedAt,
+		)
+		p = append(p,&t);
+	}
+	if rows.Err() != nil {
+		log.Printf("Err: %v",err)
+		return nil,0,fmt.Errorf("GetProfiles: %w",rows.Err);
+	}
+
+
+
+	return p,totalCount,nil
 }
